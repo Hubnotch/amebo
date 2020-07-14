6acbfff816c105b6ef1b027e1a44c90efc9dc1b9
Hubnotch <love2ekene@gmail.com>
2020-07-14 21:24:36 +0100

    first commit in amebo app

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Capture</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div class="email-capture-form">
+    <form action="index.php" class="form-body">
+        <label for="email">email :</label>
+        <input type="email" for="email" name="email">
+    </form>
+</div>    
+<form action="https://www.w3schools.com/action_page.php">
+  <div class="container">
+    <h2>Subscribe to our Newsletter</h2>
+    <p>Lorem ipsum text about why you should subscribe to our newsletter blabla. Lorem ipsum text about why you should subscribe to our newsletter blabla.</p>
+  </div>
+
+  <div class="container" style="background-color:white">
+    <input type="text" placeholder="Name" name="name" required>
+    <input type="text" placeholder="Email address" name="mail" required>
+    <label>
+      <input type="checkbox" checked="checked" name="subscribe"> Daily Newsletter
+    </label>
+  </div>
+
+  <div class="container">
+    <input type="submit" value="Subscribe">
+  </div>
+</form>
+
+
+    <?php
+    
+    
+    
+    ?>
+
+
+
+</body>
+</html>
+
